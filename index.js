5e494e79446fc2e8e7af3230bb22eaee0d72b693
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2023-11-28 15:05:59 -0600

    uPdate

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
 // DOM Elements
-const colorSchemeInputEl = document.querySelector('.color-scheme__input');
-const colorSchemeSelectorEl = document.querySelector('.color-scheme__selector');
-const colorSchemeBtn = document.querySelector('.color-scheme__button');
-const colorSchemeDisplayEl = document.querySelector('.color-scheme__display')
+const colorSchemeInputEl = document.querySelector(".color-scheme__input");
+const colorSchemeSelectorEl = document.querySelector(".color-scheme__selector");
+const colorSchemeBtn = document.querySelector(".color-scheme__button");
+const colorSchemeDisplayEl = document.querySelector(".color-scheme__display");
 
 // Variables
-let selectedColor = 'f55a5a'; //Default color
-let selectedScheme = 'monochrome'; //Default scheme
+let selectedColor = "f55a5a"; //Default color
+let selectedScheme = "monochrome"; //Default scheme
 
 // Function Variables
 const initializePage = () => getColorSchemeFromApi();
-const updateSelectedColor = () => selectedColor = colorSchemeInputEl.value.slice(1); //Slice removes '#'
-const updateSelectedScheme = () => selectedScheme = colorSchemeSelectorEl.value;
+const updateSelectedColor = () =>
+  (selectedColor = colorSchemeInputEl.value.slice(1)); //Slice removes '#'
+const updateSelectedScheme = () =>
+  (selectedScheme = colorSchemeSelectorEl.value);
 
 // Function Declarations
 function getColorSchemeFromApi() {
-    colorSchemeDisplayEl.innerHTML = '';
-    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedScheme}`)
-        .then(res => res.json())
-        .then(data => {
-            colorSchemeDisplayEl.innerHTML = data.colors.map(color => `
+  colorSchemeDisplayEl.innerHTML = "";
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedScheme}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      colorSchemeDisplayEl.innerHTML = data.colors
+        .map(
+          (color) => `
                 <div class='display-container'>
                     <div class='display-color' style='background-color: ${color.hex.value}'></div>
                     <div class='display-title'>${color.hex.value}</div>
-                </div>`).join('')
-        });
+                </div>`
+        )
+        .join("");
+    });
 }
 
 function handleTitleClick(event) {
-    if (event.target.classList.contains('display-title')) {
-        navigator.clipboard.writeText(event.target.textContent)
-            .then(() => alert('Copied to clipboard'));
-    }
+  if (event.target.classList.contains("display-title")) {
+    navigator.clipboard
+      .writeText(event.target.textContent)
+      .then(() => alert("Copied to clipboard"));
+  }
 }
 
 // Event Listeners
-initializePage() // document.addEventListener('DOMContentLoaded', initializePage)
-colorSchemeDisplayEl.addEventListener('click', handleTitleClick);
-colorSchemeBtn.addEventListener('click', function() {
-    updateSelectedColor(),
-    updateSelectedScheme(),
-    getColorSchemeFromApi()
+initializePage(); // document.addEventListener('DOMContentLoaded', initializePage)
+colorSchemeDisplayEl.addEventListener("click", handleTitleClick);
+colorSchemeBtn.addEventListener("click", function () {
+  updateSelectedColor(), updateSelectedScheme(), getColorSchemeFromApi();
 });
